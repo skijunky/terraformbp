8370c5fdb224246721337614febecee0ffe1a5b6
skijunky <43769734+skijunky@users.noreply.github.com>
2022-11-18 13:45:10 -0800

    Update machine.tf

--- vsphere-machine/machine.tf
+++ vsphere-machine/machine.tf
@@ -1,77 +1,50 @@
-terraform {
-  required_version = ">= 0.13.0"
-  required_providers {
-    vsphere = {
-      source = "vsphere"
-      version = ">= 1.24.3"
-    }
-  }
+variable "vm_name" {
+  type        = string
+  description = "VM Name"
 }
 
 provider "vsphere" {
- user = var.vsphere_user
- password = var.vsphere_password
- vsphere_server = var.vsphere_server
+
+  # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-  name = var.vsphere_datacenter
-}
-
-data "vsphere_virtual_machine" "template" {
-  name          = var.vm_template
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name = "Datacenter"
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "datastore1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "Cluster/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = var.vsphere_network
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "vlan55"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_hostname
-  datastore_id     = data.vsphere_datastore.datastore.id
+  name             = var.vm_name
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-  num_cpus = var.vm_cpu_count
-  memory   = var.vm_ram_count
-  guest_id = data.vsphere_virtual_machine.template.guest_id
-
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  num_cpus = 1
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
 
   network_interface {
-    network_id = data.vsphere_network.network.id
-    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    network_id = "${data.vsphere_network.network.id}"
   }
 
   disk {
-    label            = "disk0"
-    size             = data.vsphere_virtual_machine.template.disks.0.size
-    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
-    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    label = "disk0"
+    size  = 10
   }
 
-  clone {
-    template_uuid = data.vsphere_virtual_machine.template.id
-    linked_clone = var.vm_linked_clone
-
-    customize {
-      linux_options {
-        host_name = var.vm_hostname
-        domain = var.vm_domain
-      }
-
-      network_interface {
-        ipv4_address = var.vm_ipv4_address
-        ipv4_netmask = var.vm_ipv4_netmask
-      }
-
-      ipv4_gateway = var.vm_ipv4_gateway
-    }
-  }
+  wait_for_guest_net_timeout    = -1
 }
