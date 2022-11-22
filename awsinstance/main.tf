3cf423077ea1c9c817adb013395a16b4783d4349
skijunky <43769734+skijunky@users.noreply.github.com>
2022-11-22 11:24:45 -0800

    updated main.mf
    Commented out 2nd ec2

--- awsinstance/main.tf
+++ awsinstance/main.tf
@@ -22,14 +22,14 @@ resource "aws_instance" "machine1" {
     }
 }
 
-resource "aws_instance" "machine2" {
-    ami           = "ami-0a63cd87767e10ed4"
-    instance_type = "t2.micro"
-    availability_zone = "us-west-1b"
-    tags = {
-      "type" = var.myTag
-    }
-}
+#resource "aws_instance" "machine2" {
+#    ami           = "ami-0a63cd87767e10ed4"
+#    instance_type = "t2.micro"
+#    availability_zone = "us-west-1b"
+#    tags = {
+#      "type" = var.myTag
+#    }
+#}
 
 #resource "aws_network_interface_sg_attachment" "sg_attachment1" {
 #  security_group_id    = "sg-8e01a2fb"
